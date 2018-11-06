fortuneguy97/Ky

commit a7c71b5728adb5efab59f4057caf83a82453ba4d

Add `searchParams` option (#57)

index.js
@@ -113,6 +113,7 @@ class Ky {
 		timeout = 10000,
 		hooks,
 		throwHttpErrors = true,
+		searchParams,
 		json,
 		...otherOptions
 	}) {
@@ -134,7 +135,16 @@ class Ky {
 			this._options.prefixUrl += '/';
 		}
 
-		this._input = this._options.prefixUrl + this._input;
+		const url = new _globalThis.URL(this._options.prefixUrl + this._input);
+		if (typeof searchParams === 'string' || searchParams instanceof _globalThis.URLSearchParams) {
+			url.search = searchParams;
+		} else if (searchParams && Object.values(searchParams).every(param => typeof param === 'number' || typeof param === 'string')) {
+			url.search = new _globalThis.URLSearchParams(searchParams).toString();
+		} else if (searchParams) {
+			throw new Error('`searchParams` option must be either a string, URLSearchParams instance or an object with string and number values');
+		}
+		this._input = url.toString();
+
 		this._timeout = timeout;
 		this._hooks = deepMerge({
 			beforeRequest: [],

index.d.ts
@@ -62,6 +62,12 @@ export interface Options extends RequestInit {
 	* The prefix can be any valid URL, either relative or absolute.
 	*/
 	prefixUrl?: URL | string;
+
+	/**
+	* Search parameters to include in the request URL.
+	* Setting this will override all existing search parameters in the input URL.
+	*/
+	searchParams?: string | [string, string | number] | URLSearchParams;
 }
 
 /**

test/main.js
@@ -139,6 +139,26 @@ test('timeout option', async t => {
 	await server.close();
 });
 
+test('searchParams option', async t => {
+	const server = await createTestServer();
+
+	server.get('/', (request, response) => {
+		response.end(request.url.slice(1));
+	});
+
+	const stringParams = '?pass=true';
+	const objectParams = {pass: 'true'};
+	const invalidObjectParams = {pass: ['true', 'false']};
+	const searchParams = new global.URLSearchParams(stringParams);
+
+	t.is(await ky(server.url, {searchParams: stringParams}).text(), stringParams);
+	t.is(await ky(server.url, {searchParams: objectParams}).text(), stringParams);
+	t.is(await ky(server.url, {searchParams}).text(), stringParams);
+	t.throws(() => ky(server.url, {searchParams: invalidObjectParams}), /`searchParams` option must be/);
+
+	await server.close();
+});
+
 test('throwHttpErrors option', async t => {
 	const server = await createTestServer();
 	server.get('/', (request, response) => {

commit 41ca1fc05157669f94987d6c44801c8a5366cf08

Fix relative URL resolution (#59)
Fixes https://github.com/sindresorhus/ky/issues/58

There was a regression introduced in https://github.com/sindresorhus/ky/commit/a7c71b5728adb5efab59f4057caf83a82453ba4d which broke the ability for `input` to be a relative URL like `foo` or `/foo`. The root cause is that [`window.URL`](https://developer.mozilla.org/en-US/docs/Web/API/URL) unfortunately does not support relative URLs without an explicit and absolute `base` argument. This fixes that and adds a test for it. Now that we have Cypress set up, we can properly test anything related to relative URLs.

The goal is to follow the URL resolution rules the same way that `fetch` does. So we resolve `input` against [`document.baseURI`](https://developer.mozilla.org/en-US/docs/Web/API/Node/baseURI#The_base_URL_of_a_document).

cypress/integration/index.js
@@ -6,4 +6,9 @@ describe('ky', () => {
 			ky('/foo', {prefixUrl: '/'});
 		}).to.throw(Error, /must not begin with a slash/);
 	});
+
+	it('resolves relative URLs for `input` and `prefixUrl`', async () => {
+		expect(await ky('/cypress/fixtures/fixture.json').json()).to.deep.equal({foo: true});
+		expect(await ky('fixtures/fixture.json', {prefixUrl: '/cypress/'}).json()).to.deep.equal({foo: true});
+	});
 });

index.js
@@ -135,7 +135,7 @@ class Ky {
 			this._options.prefixUrl += '/';
 		}
 
-		const url = new _globalThis.URL(this._options.prefixUrl + this._input);
+		const url = new _globalThis.URL(this._options.prefixUrl + this._input, document.baseURI);
 		if (typeof searchParams === 'string' || searchParams instanceof _globalThis.URLSearchParams) {
 			url.search = searchParams;
 		} else if (searchParams && Object.values(searchParams).every(param => typeof param === 'number' || typeof param === 'string')) {

test/_require.js
@@ -6,3 +6,6 @@ global.Headers = Headers;
 global.Response = Response;
 global.URL = URL;
 global.URLSearchParams = URLSearchParams;
+global.document = {
+	baseURI: 'https://example.com'
+};